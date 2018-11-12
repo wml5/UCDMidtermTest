cf9cbcb7dddedf717595fb24ef4998338ecccda2
wml5 <wml5@FSTU603026.pti.edu>
2018-11-12 14:53:05 -0500

    Commit 1

---
+++ src/midtermtest/MidtermTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermtest;
+
+public class MidtermTest 
+{
+
+    public static void main(String[] args) 
+    {
+        int number1 = 1;
+        int number2 = 2;
+        int answer;
+        
+        answer = number1 + number2;
+        
+        System.out.print("The answer to " + number1 + " + " + number2 + " is ");
+        System.out.println(answer);
+    }
+    
+}
